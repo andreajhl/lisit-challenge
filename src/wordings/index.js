d8c45b6cf0aca89cf6c326889c21557a62e3a606
Andrea Hernandez <andreajhl29@gmail.com>
2024-02-12 01:31:44 -0300

    feat: :sparkles: add wordings

---
+++ src/wordings/index.js
@@ -0,0 +1,74 @@
+const wordings = {
+  navbar: {
+    title: "Starpedia",
+    placeholder: 'Enter a'
+  },
+  home: {
+    title: "Learn more about",
+    categories: {
+      people: "Characters",
+      planets: "Planets",
+      starships: "Starships"
+    }
+  },
+  categories: {
+    films: "Films:",
+    people: {
+      title: "List of Characters"
+    },
+    starships: {
+      title: "List of Starships"
+    },
+    planets: {
+      title: "List of Planets"
+    }
+  },
+  details: {
+    characteristics: {
+      title: "Characteristics:",
+      birthYear: "Birth Year:",
+      eye: "Eye Color:",
+      hair: "Hair Color:",
+      height: "Height:",
+      mass: "Mass:",
+      skin: "Skin Color:",
+      model: "Model:",
+      class: "Class:",
+      length: "Length:",
+      manufacturer: "Manufacturer:",
+      costCredits: "Cost in Credits:",
+      cargoCapacity: "Cargo Capacity:",
+      passengers: "Passengers:",
+      atmosphereSpeed: "Max Atmosphering Speed:",
+      hyperdriveRating: "Hyperdrive Rating:",
+      consumables: "Consumables:",
+      diameter: "Diameter:",
+      climate: "Climate:",
+      gravity: "Gravity:",
+      terrain: "Terrain:",
+      population: "Population:",
+      vehicles: "Vehicles:",
+      surfaceWater: "Surface Water:",
+      producer: "Producer:",
+      premiere: "Premiere:",
+      episode: "Episodes",
+      description: "Description:"
+    },
+    additional: {
+      films: "Films:",
+      starships: "Starships:",
+      pilots: "Pilots:",
+      residents: "Residents:"
+    }
+  },
+  notFound: {
+    title: "Oops!",
+    message: "Sorry. We can't find the page",
+    retry: "Return to Start"
+  },
+  warnings: {
+    loading: "Loading",
+  }
+};
+
+export default wordings;
