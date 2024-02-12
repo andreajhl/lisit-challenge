045e543133aaccd6c457c5f436a1f05410258a6a
Andrea Hernandez <andreajhl29@gmail.com>
2024-02-12 01:34:57 -0300

    feat: :sparkles: add constants

---
+++ src/constants/category.js
@@ -0,0 +1,6 @@
+export const FILMS = 'films';
+export const PILOTS = 'pilots';
+export const PEOPLE = 'people';
+export const PLANETS = 'planets';
+export const RESIDENTS = 'residents';
+export const STARSHIPS = 'starships';
