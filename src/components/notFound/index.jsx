9dbae3721fe3d45d30d4bf093775c1000d8511cb
Andrea Hernandez <andreajhl29@gmail.com>
2024-02-12 02:56:58 -0300

    implements cardEmpty

--- src/components/notFound/index.jsx
+++ src/components/notFound/index.jsx
@@ -1,8 +1,7 @@
-
-import { Link, useRouteError } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
 import wordings from '../../wordings';
 import Layout from '../layout';
-import './styles.scss';
+import CardEmpty from '../ui/cardEmpty';
 
 const NotFound = () => {
   const { notFound } = wordings;
@@ -10,22 +9,11 @@ const NotFound = () => {
 
   return (
     <Layout>
-      <main className='error-boundary'>
-        <div className='error-boundary__content'>
-          <h1 className='error-boundary__content-title'>{notFound.title}</h1>
-          <div className='error-boundary__content-message'>
-            <p className='error-boundary__content-message-code'>
-              {response.status || 404}
-            </p>
-            <p className='error-boundary__content-message-text'>{notFound.message}</p>
-          </div>
-          <div className='error-boundary__content-retry'>
-            <Link to='/' className='error-boundary__content-retry-link'>
-              {notFound.retry} &rarr;
-            </Link>
-          </div>
-        </div>
-      </main>
+      <CardEmpty
+        link='/'
+        {...notFound}
+        code={response?.status || 404}
+      />
     </Layout>
   );
 };
